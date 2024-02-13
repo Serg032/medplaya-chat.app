2c7be1053a87ab41e6049dc79dd7615ec84f6111
Serg032 <sergioradigales@gmail.com>
2024-02-13 10:36:20 +0100

    api call to custom openai endpoint working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { ChatComponent } from './chat/chat.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, ChatComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, CommonModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+interface ChatResponse {
+  chatMessage: string;
+}
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
 })
 export class ChatComponent {
+  public messageInput = new FormControl();
 
+  public async sendMessage() {
+    try {
+      const response = await fetch('http://localhost:3000', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: this.messageInput.value }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const responseData: ChatResponse = await response.json();
+      // this.responseMessage = responseData; // Assuming your response is a string
+
+      console.log('Response', responseData.chatMessage);
+    } catch (error) {
+      console.log(error);
+    }
+
+    this.messageInput.setValue('');
+  }
 }
