dd513e8064ac5397241390c5e4a7f5b4a804179e
Serg032 <sergioradigales@gmail.com>
2024-03-19 11:43:34 +0100

    fixes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   aiDataUrl: 'https://claudiafunc.azurewebsites.net/api/medplayafunc',
-  apiKey: '8OM1OEwq1y2GOVfvYfQB6yXQbOB_5KhSoSYaZ3zgv4chAzFugGvudg=',
+  apiKey: '8OM1OEwq1y2GOVfvYfQB6yXQbOB_5KhSoSYaZ3zgv4chAzFugGvudg==',
 };
