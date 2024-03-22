1072f1c734dba3c96208b8373be335f022b461c5
Serg032 <sergioradigales@gmail.com>
2024-03-22 12:32:22 +0100

    data assitant url updated and added to environments

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 interface RequestPayload {
   question: string;
@@ -11,20 +12,18 @@ type Hotel = 'Calypso';
   providedIn: 'root',
 })
 export class DataApiService {
+  private rootUrl = environment.chatAssistantUrl;
   constructor() {}
 
   public async makeQuestion(question: string) {
-    const response = await fetch(
-      'https://claudiafunc.azurewebsites.net/api/medplayafunc',
-      {
-        method: 'POST',
-        headers: {
-          'x-functions-key':
-            '8OM1OEwq1y2GOVfvYfQB6yXQbOB_5KhSoSYaZ3zgv4chAzFugGvudg==',
-        },
-        body: JSON.stringify(this.buildPayload(question)),
-      }
-    );
+    const response = await fetch(environment.chatAssistantUrl, {
+      method: 'POST',
+      headers: {
+        'x-functions-key':
+          '8OM1OEwq1y2GOVfvYfQB6yXQbOB_5KhSoSYaZ3zgv4chAzFugGvudg==',
+      },
+      body: JSON.stringify(this.buildPayload(question)),
+    });
 
     return response.text();
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://medplaya-nestjs-back.azurewebsites.net',
+  chatAssistantUrl:
+    'https://medpalyafunc-dev.azurewebsites.net/api/medplayafunc',
 };
