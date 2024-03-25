4debfb556a6b898cb845569df3bbfb2a33c36682
Serg032 <sergioradigales@gmail.com>
2024-03-25 10:35:59 +0100

    data api url and api key  changed

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 interface RequestPayload {
   question: string;
@@ -14,17 +15,14 @@ export class DataApiService {
   constructor() {}
 
   public async makeQuestion(question: string) {
-    const response = await fetch(
-      'https://claudiafunc.azurewebsites.net/api/medplayafunc',
-      {
-        method: 'POST',
-        headers: {
-          'x-functions-key':
-            '8OM1OEwq1y2GOVfvYfQB6yXQbOB_5KhSoSYaZ3zgv4chAzFugGvudg==',
-        },
-        body: JSON.stringify(this.buildPayload(question)),
-      }
-    );
+    const response = await fetch(environment.chatAssistantUrl, {
+      method: 'POST',
+      headers: {
+        'x-functions-key':
+          'nweHKZRxXM81Qex1p-__fzEUJ5bt1w-tortOdB7Vv1BdAzFuEvmtHw==',
+      },
+      body: JSON.stringify(this.buildPayload(question)),
+    });
 
     return response.text();
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://medplaya-nestjs-back.azurewebsites.net',
+  chatAssistantUrl:
+    'https://claudiafunc-dev.azurewebsites.net/api/medplayafunc',
 };
